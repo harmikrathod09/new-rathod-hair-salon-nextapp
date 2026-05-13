a512b856b3b9de11f951bf81651a9f054005410d
harmik0919 <harmikrathod56@gmail.com>
2026-05-13 18:31:47 +0530

    meta ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-3327652769075609"></meta>
+      </head>
       <body className="min-h-full flex flex-col">
         {children}
         <Script
